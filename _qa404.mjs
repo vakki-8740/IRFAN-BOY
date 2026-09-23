c97d556a747afbc9fdf14f0db6b61f56062617f0
vakki-8740 <lightsir8740@gmail.com>
2026-09-23 23:15:11 +0530

    Delete all files from repository

--- _qa404.mjs
+++
@@ -1,8 +0,0 @@
-export default async function run(page, ui) {
-  const file404 = [];
-  page.on('response', r => { if (r.status() === 404) file404.push(r.url()); });
-
-  await page.goto('http://127.0.0.1:8099/admin/index.html', { waitUntil: 'networkidle' });
-  await page.waitForTimeout(1500);
-  return { file404 };
-}
